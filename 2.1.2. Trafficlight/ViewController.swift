40499d3d1ef3dc41d710011e1fb3ac0ce6441d95
Igor Solodyankin <solodyankin.ie@ya.ru>
2022-02-20 22:28:47 +0500

    Final

--- 2.1.2. Trafficlight/ViewController.swift
+++ 2.1.2. Trafficlight/ViewController.swift
@@ -7,30 +7,57 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
     
     @IBOutlet var redView: UIView!
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenView: UIView!
+    
     @IBOutlet var toggleButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redView.layer.cornerRadius = self.redView.frame.height / 2
-        yellowView.layer.cornerRadius = self.yellowView.frame.height / 2
+        redView.layer.cornerRadius = redView.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.frame.height / 2
         greenView.layer.cornerRadius = self.greenView.frame.height / 2
         
-        redView.alpha = 0.3
-        yellowView.alpha = 0.3
-        greenView.alpha = 0.3
+        redView.alpha = lightIsOff
+        yellowView.alpha = lightIsOff
+        greenView.alpha = lightIsOff
+        
+        toggleButton.layer.cornerRadius = 10
         
         // Do any additional setup after loading the view.
     }
 
     @IBAction func colourSwicthButton(_ sender: Any) {
+        toggleButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            greenView.alpha = lightIsOff
+            redView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redView.alpha = lightIsOff
+            yellowView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowView.alpha = lightIsOff
+            greenView.alpha = lightIsOn
+            currentLight = .red
+        }
         
     }
     
