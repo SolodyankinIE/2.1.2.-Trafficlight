0f5e1599c0b45fe5fa45c6e923fe4d5bfcf2c176
Igor Solodyankin <solodyankin.ie@ya.ru>
2022-02-20 21:47:29 +0500

    Initial Commit

---
+++ 2.1.2. Trafficlight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.1.2. Trafficlight
+//
+//  Created by Игорь Солодянкин on 20.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
