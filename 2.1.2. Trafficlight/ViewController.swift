ff7a4b02e96afc86c644a213474a01b5fd9afbfa
Igor Solodyankin <solodyankin.ie@ya.ru>
2022-02-20 22:10:00 +0500

    Main screen

--- 2.1.2. Trafficlight/ViewController.swift
+++ 2.1.2. Trafficlight/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var toggleButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        redView.layer.cornerRadius = self.redView.frame.height / 2
+        yellowView.layer.cornerRadius = self.yellowView.frame.height / 2
+        greenView.layer.cornerRadius = self.greenView.frame.height / 2
+        
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func colourSwicthButton(_ sender: Any) {
+        
+    }
+    
 }
 
